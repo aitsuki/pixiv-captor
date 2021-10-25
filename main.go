817aa77387e72a41cbcf6d19149f101548ce7999
aitsuki <aitsukigo@gmail.com>
2021-10-25 18:53:28 +0800

    Fix default path

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"path/filepath"
 
 	"github.com/aitsuki/pixiv-captor/services"
 )
@@ -16,11 +17,16 @@ func main() {
 	flag.IntVar(&port, "P", 8080, "端口")
 	flag.StringVar(&username, "u", "", "用户名")
 	flag.StringVar(&password, "p", "", "密码")
-	flag.StringVar(&dbPath, "db", "./pixiv.db", "数据库保存位置")
-	flag.StringVar(&logPath, "log", "./pixiv.log", "日志保存位置")
+	flag.StringVar(&dbPath, "db", relativePath("pixiv.db"), "数据库保存位置")
+	flag.StringVar(&logPath, "log", relativePath("pixiv.log"), "日志保存位置")
 	flag.Parse()
 	if len(username) == 0 || len(password) == 0 {
 		log.Fatal("为了安全性考虑，请设置管理员账户和密码。")
 	}
 	log.Fatal(services.Run(port, dbPath, logPath, username, password))
+}
+
+func relativePath(name string) string {
+	path, _ := filepath.Abs(name)
+	return path
 }
